4a666e193ea2dc4c95a3ec806b0c37d0708528b1
Irandufa Indebu <irandufa@gmail.com>
2024-12-13 21:47:22 +0300

    modularized

---
+++ scripts/sentment_analyzer.py
@@ -0,0 +1,28 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+# download nltk corpus (first time only)
+import nltk
+nltk.download('all')
+def preprocess_text(text):
+    # Tokenize the text
+    tokens = word_tokenize(text.lower())
+    # Remove stop words
+    filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
+    # Lemmatize the tokens
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+    # Join the tokens back into a string
+    processed_text = ' '.join(lemmatized_tokens)
+    return processed_text
+# apply the function df
+# initialize NLTK sentiment analyzer
+analyzer = SentimentIntensityAnalyzer()
+def get_sentiment(text):
+    scores = analyzer.polarity_scores(text)
+    sentiment = 1 if scores['pos'] > 0 else 0
+    return sentiment
+sample="This product is so interesting"
+print(get_sentiment(sample))
